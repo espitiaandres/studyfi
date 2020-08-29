f7e5e7b2e3e7bd48753a0061f8400ae4a90e4ad3
espitiaandres <espitiaandres123@gmail.com>
2020-08-28 22:50:01 -0400

    updating redirect URI for production

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "53bcecd410634483b5388541c159849d";
-export const redirectUri = "http://localhost:3000/";
-// export const redirectUri = "http://www.andres-espitia.com";
+// export const redirectUri = "http://localhost:3000/";
+export const redirectUri = "http://www.andres-espitia.com/studyfi";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",

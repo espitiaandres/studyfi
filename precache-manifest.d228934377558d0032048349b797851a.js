ad7f6df0620e9cd4973b24d6703148f0bab3592f
espitiaandres <espitiaandres123@gmail.com>
2020-08-28 22:48:25 -0400

    Updates

---
+++ precache-manifest.d228934377558d0032048349b797851a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe194c88283de95f7050134820d45abb",
+    "url": "/studyfi/index.html"
+  },
+  {
+    "revision": "71665525aaab5cb2c7e9",
+    "url": "/studyfi/static/css/main.41d9fdac.chunk.css"
+  },
+  {
+    "revision": "976a55419ca01d5d576f",
+    "url": "/studyfi/static/js/2.61d0bd92.chunk.js"
+  },
+  {
+    "revision": "26a196f4f05f604044003a197f3c9757",
+    "url": "/studyfi/static/js/2.61d0bd92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71665525aaab5cb2c7e9",
+    "url": "/studyfi/static/js/main.d78250db.chunk.js"
+  },
+  {
+    "revision": "af8270e35460579a4ac4",
+    "url": "/studyfi/static/js/runtime-main.5f7e87f6.js"
+  },
+  {
+    "revision": "4ab11064d6b8bac6d0be5efce763a056",
+    "url": "/studyfi/static/media/sketchyFavicon.4ab11064.png"
+  }
+]);

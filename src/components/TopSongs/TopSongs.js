75ae1c7b001876024308941c6ad0b56dcb114fbf
espitiaandres <espitiaandres123@gmail.com>
2020-09-30 23:08:44 -0400

    added table of top songs

--- src/components/TopSongs/TopSongs.js
+++ src/components/TopSongs/TopSongs.js
@@ -8,12 +8,15 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
 import hash from '../../utils/hash';
 import './TopSongs.css';
 
-function TopSongs() {
+const TopSongs = () => {
     const token = hash.access_token;
     let filteredTopTracksData = [];
+    const [filteredData, setFilteredData] = useState([]);
+
 
     useEffect(() => {
         let audioFeaturesTracksIDs = "";
@@ -29,7 +32,7 @@ function TopSongs() {
                 time_range: 'long_term'
             }
         }).then(({ data }) => {
-            data.items.map(song => {
+            data.items.map((song) => {
                 let allArtists = "";
                 const topTrackFiltered = {};        
                 topTrackFiltered.name = song.name;
@@ -59,25 +62,100 @@ function TopSongs() {
                 params: {
                     ids: audioFeaturesTracksIDs
                 }
-            }).then((response) => {
-                filteredTopTracksData.map((track) => {
-                    response.data.audio_features.map((feature) => {
-                        track.acousticness = feature.acousticness;
-                        track.danceability = feature.danceability;
-                        track.energy = feature.energy;
-                        track.key = feature.key
-                        track.instrumentalness = feature.instrumentalness;
-                        track.valence = feature.valence;
-                    })
-                })
-                console.log(filteredTopTracksData);
+            }).then(({ data: { audio_features } }) => {
+                for (let i = 0; i < filteredTopTracksData.length; i++) {
+                    filteredTopTracksData[i].acousticness = audio_features[i].acousticness;
+                    filteredTopTracksData[i].danceability = audio_features[i].danceability;
+                    filteredTopTracksData[i].energy = audio_features[i].energy;
+                    filteredTopTracksData[i].keySignature = audio_features[i].key;
+                    filteredTopTracksData[i].instrumentalness = audio_features[i].instrumentalness;
+                    filteredTopTracksData[i].valence = audio_features[i].valence;
+                    filteredTopTracksData[i].key = i + 1;
+                }
+
+                setFilteredData(filteredTopTracksData);
             })
         })
     }, []);
 
+    function formatter(cell, row) {     
+        return cell.toString().includes("http") ? 
+        <span>
+            <img src={cell}/>
+        </span> : 
+        <span>
+            {cell}
+        </span>;
+    }
+
+    const columns = [
+        {
+            text: 'Name',
+            dataField: 'name',
+            formatter
+        },
+        {
+            text: 'Album',
+            dataField: 'album',
+            formatter
+        },
+        {
+            text: 'Album Cover',
+            dataField: 'albumCover',
+            formatter
+        },
+        {
+            text: 'Release Date',
+            dataField: 'releaseDate',
+            formatter
+        },
+        {
+            text: 'Artists',
+            dataField: 'artists',
+            formatter
+        },
+        {
+            text: 'Acousticness',
+            dataField: 'acousticness',
+            formatter
+        },
+        {
+            text: 'Danceability',
+            dataField: 'danceability',
+            formatter
+        },
+        {
+            text: 'Energy',
+            dataField: 'energy',
+            formatter
+        },
+        {
+            text: 'KeySignature',
+            dataField: 'keySignature',
+            formatter
+        },
+        {
+            text: 'Instrumentalness',
+            dataField: 'instrumentalness',
+            formatter
+        },
+        {
+            text: 'Valence',
+            dataField: 'valence',
+            formatter
+        }
+    ]
+
     return (
         <div>
             <h1>your most listened to artists and tracks</h1>
+            <div>
+                <BootstrapTable
+                    keyField="id"
+                    data={filteredData}
+                    columns={columns}
+                />
+            </div>
         </div>
     )
 }

aaca81715107ed8b325363306b077e6e4aa0e466
espitiaandres <espitiaandres123@gmail.com>
2020-11-01 22:52:13 -0500

    switched back to dev env

--- src/utils/config.js
+++ src/utils/config.js
@@ -8,8 +8,8 @@
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "53bcecd410634483b5388541c159849d";
-// export const redirectUri = "http://localhost:3000/";                    // dev redirectUri
-export const redirectUri = "https://www.andres-espitia.com/studyfi/";      // prod redirectUri
+export const redirectUri = "http://localhost:3000/";                    // dev redirectUri
+// export const redirectUri = "https://www.andres-espitia.com/studyfi/";      // prod redirectUri
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",

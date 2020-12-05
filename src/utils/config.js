22b2f7a7eefed752ab921c81baeed1c3aab41b2a
espitiaandres <espitiaandres123@gmail.com>
2020-12-05 12:39:20 -0500

    refactored config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,14 +6,21 @@
 //  Copyright © 2020 Andres Espitia. All rights reserved.
 //
 
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = "53bcecd410634483b5388541c159849d";
-// export const redirectUri = "http://localhost:3000/";                    // dev redirectUri
-export const redirectUri = "https://www.andres-espitia.com/studyfi/";      // prod redirectUri
-export const scopes = [
+const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "53bcecd410634483b5388541c159849d";
+// const redirectUri = "http://localhost:3000/";                    // dev redirectUri
+const redirectUri = "https://www.andres-espitia.com/studyfi/";      // prod redirectUri
+const scopes = [
     "user-top-read",
     "user-read-currently-playing",
     "user-read-playback-state",
     "user-modify-playback-state",
     "playlist-read-private"
 ];
+
+export { 
+    authEndpoint, 
+    clientId, 
+    redirectUri, 
+    scopes 
+}

68adecbfbc2a7522e7fd26b4142400090b1c24e2
Andres Espitia <espitiaandres123@gmail.com>
2022-09-20 17:15:54 -0400

    wip

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -2,16 +2,22 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>
-      <div className="text-4xl text-primary">Studyfi</div>
+    <div className="">
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <div className="grid grid-cols-1 place-content-around content-center">
+          <div className="text-8xl text-primary">Studyfi</div>
 
-      <button className="btn btn-outline btn-primary">
-        Login with Spotify
-      </button>
+          <div className="flex flex-col justify-center items-center">
+            <button className="btn btn-outline btn-primary">
+              Login with Spotify
+            </button>
+          </div>
+        </div>
 
-      <div className="flex flex-row justify-evenly">
-        <div>Built by</div>
-        <a href="github.com/espitiaandres"> espitiaandres</a>
+        <div className="fixed bottom-20 left-20 text-primary">
+          <div>Built by</div>
+          <a href="https://github.com/espitiaandres">espitiaandres</a>
+        </div>
       </div>
     </div>
   );

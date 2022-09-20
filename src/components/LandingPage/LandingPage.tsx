8f0bcad8690cf49636c0c172e1628d1f48ad93c8
Andres Espitia <espitiaandres123@gmail.com>
2022-09-20 17:22:27 -0400

    wip

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -2,23 +2,25 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className="">
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <div className="grid grid-cols-1 place-content-around content-center">
-          <div className="text-8xl text-primary">Studyfi</div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="grid grid-cols-1 place-content-around h-128">
+        <div className="text-8xl text-primary">Studyfi</div>
 
-          <div className="flex flex-col justify-center items-center">
-            <button className="btn btn-outline btn-primary">
-              Login with Spotify
-            </button>
-          </div>
-        </div>
-
-        <div className="fixed bottom-20 left-20 text-primary">
-          <div>Built by</div>
-          <a href="https://github.com/espitiaandres">espitiaandres</a>
+        <div className="flex flex-col justify-center items-center">
+          <button className="btn btn-outline btn-primary">
+            Login with Spotify
+          </button>
         </div>
       </div>
+      <div className="">
+        <div>Built by</div>
+        <a href="https://github.com/espitiaandres">espitiaandres</a>
+      </div>
+
+      {/* <div className="fixed bottom-20 left-20 text-primary">
+        <div>Built by</div>
+        <a href="https://github.com/espitiaandres">espitiaandres</a>
+      </div> */}
     </div>
   );
 };
